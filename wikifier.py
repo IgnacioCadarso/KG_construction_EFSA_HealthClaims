264a2f9ed85681f4124577fc633ff44b573361b4
IgnacioCadarso <i.cadarsocerdeirina@student.maastrichtuniversity.nl>
2023-03-06 19:37:23 +0100

    Initial commit

---
+++ wikifier.py
@@ -0,0 +1,21 @@
+import urllib.parse, urllib.request, json
+
+def CallWikifier(text, lang="en", threshold=0.8):
+    # Prepare the URL.
+    data = urllib.parse.urlencode([
+        ("text", text), ("lang", lang),
+        ("userKey", "zuseqohhtjiksmyksrxruvssndndxj"),
+        ("pageRankSqThreshold", "%g" % threshold), ("applyPageRankSqThreshold", "true"),
+        ("nTopDfValuesToIgnore", "200"), ("nWordsToIgnoreFromList", "200"),
+        ("wikiDataClasses", "true"), ("wikiDataClassIds", "false"),
+        ("support", "true"), ("ranges", "false"), ("minLinkFrequency", "2"),
+        ("includeCosines", "false"), ("maxMentionEntropy", "3")
+        ])
+    url = "http://www.wikifier.org/annotate-article"
+    # Call the Wikifier and read the response.
+    req = urllib.request.Request(url, data=data.encode("utf8"), method="POST")
+    with urllib.request.urlopen(req, timeout = 60) as f:
+        response = f.read()
+        response = json.loads(response.decode("utf8"))
+    # Output the annotations.
+    return response
